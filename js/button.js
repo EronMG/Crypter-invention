33846bc1c058fbfe4f2279cf160ab290140bab9b
Nikita Zytsev <eronmcgonagal@gmail.com>
2023-03-21 09:36:36 +0300

    feat: end slider section

--- js/button.js
+++ js/button.js
@@ -1,3 +1,7 @@
-function viewDiv() {
-    document.getElementById("div1").style.display = "block";
-};
+function showSelectedDay() {
+    const dayOfWeekElement = document.getElementById("day-of-week");
+    const selectedValue = dayOfWeekElement.value;
+    const selectedText = dayOfWeekElement.options[dayOfWeekElement.selectedIndex].text;
+    const selectedDayElement = document.getElementById("selected-day");
+    selectedDayElement.innerHTML = `${selectedText} ${selectedValue}`;
+}
